f337b474b89848897edbafaa3de8f49219d2aa0b
Revolt27 <kuckushckin.alex@yandex.ru>
2019-03-21 22:00:12 +0300

    Добавлена реализация алгоритма сотрировки перемешиванием

--- Algoritms.py
+++ Algoritms.py
@@ -1,5 +1,7 @@
 import SortingAlgorithms.BubbleSort as BubbleSort
+import SortingAlgorithms.CocktailSort as CocktailSort
 
 lst = [9,8,6,5,2,5,77,3,5,0,1]
 
-print(BubbleSort.getSorted(lst)) 
+print(BubbleSort.sortOut(lst)) 
+print(CocktailSort.sortOut(lst))
--- SortingAlgorithms/BubbleSort.py
+++ SortingAlgorithms/BubbleSort.py
@@ -1,4 +1,4 @@
-def getSorted(collection):
+def sortOut(collection):
     steeps = frozenset(range(len(collection)))
     for item in collection:
         for steep in steeps:
