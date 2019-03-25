107a7da82b00bd3f653035adc33227200f56ca55
Alexey Kukushkin <kuckushckin.alex@yandex.ru>
2019-03-25 23:11:38 +0300

    Добавлен алгоритм Гномьей сортировки

--- Algoritms.py
+++ Algoritms.py
@@ -1,7 +1,9 @@
 import SortingAlgorithms.BubbleSort as BubbleSort
 import SortingAlgorithms.CocktailSort as CocktailSort
+import SortingAlgorithms.GnomeSort as  GnomeSort
 
 lst = [9,8,6,5,2,5,77,3,5,0,1]
 
 print(BubbleSort.sortOut(lst)) 
 print(CocktailSort.sortOut(lst))
+print(GnomeSort.sortOut(lst))
